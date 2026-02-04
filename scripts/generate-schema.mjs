d1e6c65ea0e682e232ded748e5f929459d55a7f7
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2026-02-04 07:50:49 +0100

    chore: schema

--- scripts/generate-schema.mjs
+++ scripts/generate-schema.mjs
@@ -3,7 +3,7 @@ import { createJiti } from "jiti";
 import { toJSONSchema } from "zod";
 
 const jiti = createJiti(import.meta.url, { interopDefault: true });
-const schemaModule = await jiti.import("../src/config-schema.ts");
+const schemaModule = await jiti.import("../src/config/schema.ts");
 
 const schema = toJSONSchema(schemaModule.ConfigSchema, {
 	name: "DocsCacheConfig",
